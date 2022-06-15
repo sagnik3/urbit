1dca8346be204f6649830539b63773a46cdfacb4
Sagnik Chatterjee <chatterjeesagnik33@gmail.com>
2022-06-15 14:23:03 +0530

    added changes

--- TECHINICAL-SPECIFICATION.md
+++
@@ -1,6 +0,0 @@
-## Local DB:
-
-For the purpose of developlement and project managerment using a local database called as BoltDB to check and manager resources and verify our progress.
-
-BoltDB is a simple B+ tree schema nosql database that supports upto 1TB operations for individual reads and writes on a operation.
-
--- main.go
+++ main.go
@@ -1,11 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	bolt "go.etcd.io/bbolt"
+)
 
 func description() {
 	fmt.Println("urbit v0.0.0.1 : A distributed,key-value store.")
 }
 
+//definign flags for cli usage
+
+var (
+	dbLocation = flag.String("db-location", "", "The path to the bolt DB database")
+)
+
+//validate our flags
+/*
+func parseFlags() {
+	flag.Parse()
+
+	if *dbLocation == "" {
+		//if empty then not a valid location
+		log.Fatal("ERROR: DB Location Provided is empty, must provide a DB location")
+	}
+}
+*/
 func main() {
 	description()
+
+	//	parseFlags()
+	db, err := bolt.Open(*dbLocation, 0666, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
 }
