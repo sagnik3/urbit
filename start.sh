a4f47ae703713c0eb7a82cced1e57c1dccdcf3f9
Sagnik Chatterjee <chatterjeesagnik33@gmail.com>
2022-07-01 10:32:23 +0530

    fix: added location changes

--- start.sh
+++ start.sh
@@ -19,7 +19,7 @@ urbit -db-location=db2-r.db -http-addr=127.0.0.33:8080 -config-file=sharding.tom
 urbit -db-location=db3.db -http-addr=127.0.0.4:8080 -config-file=sharding.toml -shard=DB3 &
 urbit -db-location=db3-r.db -http-addr=127.0.0.44:8080 -config-file=sharding.toml -shard=DB3 -replica &
 
-urbit -db-location=db4.db -http-addr=127.0.0.5:8080 -config-file=sharding.toml -shard=Tashkent &
-urbit -db-location=db4-r.db -http-addr=127.0.0.55:8080 -config-file=sharding.toml -shard=Tashkent -replica &
+urbit -db-location=db4.db -http-addr=127.0.0.5:8080 -config-file=sharding.toml -shard=DB4 &
+urbit -db-location=db4-r.db -http-addr=127.0.0.55:8080 -config-file=sharding.toml -shard=DB4 -replica &
 
 wait
